a89d569bba2c90f525b8bfb003ef73a19a994ec9
Kenrick Tan (TanTan) <kenrick.tan@versent.com.sg>
2021-11-25 13:31:32 +0800

    completed base without docker

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+    // @GetMapping ensures that HTTP GET Requests to /helloworld is mapped to
+    // helloworld()
+    @GetMapping("/helloworld")
+    public String helloWorld(@RequestParam(name = "name", required = false, defaultValue = "User") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "helloWorld";
+    }
+}
---
+++ src/main/java/com/example/helloworld/redirect.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class redirect {
+    
+}
