3af3593a717e9ed5577c7e7db219fdd2dbd31f81
Kenrick Tan (TanTan) <kenrick.tan@versent.com.sg>
2021-11-25 14:09:25 +0800

    new test

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.helloworld;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@SpringBootTest
-class HelloWorldApplicationTests {
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = HelloWorldController.class)
+class HelloWorldApplicationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void homePage() throws Exception {
+		// N.B. jsoup can be useful for asserting HTML content
+		mockMvc.perform(get("/index.html")).andExpect(content().string(containsString("Get your greeting")));
+	}
+
+	@Test
+	public void helloworld() throws Exception {
+		mockMvc.perform(get("/helloworld")).andExpect(content().string(containsString("Hello, World, User!")));
+	}
+
+	@Test
+	public void greetingWithUser() throws Exception {
+		mockMvc.perform(get("/helloworld").param("name", "Greg"))
+				.andExpect(content().string(containsString("Hello World, Greg!")));
 	}
 
 }
